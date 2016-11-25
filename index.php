2703a1a94d4cd9bd74296ca20ddcb4ef3a55d6c6
Fabien Basmaison <fabien@arkhi.org>
2016-11-25 16:26:05 +0800

    Add basic trail on map.

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ if ( $canAdd ) {
     $point->addPoint( $point->db, $point->data );
 } else {
     $points = $point->getPoints( $point->db );
+    $lastPoint = $points[ count( $points ) - 1 ];
 }
 
 ?>
@@ -22,29 +23,82 @@ if ( $canAdd ) {
 <head>
     <meta charset="utf-8">
     <title>Breadcrumbs</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.2/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.0.2/dist/leaflet.js"></script>
+    <script src="vendor/leaflet-providers.js"></script>
+
+    <style>
+        #map {
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+        }
+    </style>
 </head>
 <body>
-    <h1>Breadcrumbs</h1>
-    <p>Where am I?</p>
+    <header>
+        <h1>Breadcrumbs</h1>
+        <p>Where am I?</p>
+    </header>
 
-    <?php if ( $canAdd ) : ?>
-        <pre><?php print_r( $pointParams ); ?></pre>
-    <?php else : ?>
-        <table>
-            <tr>
-                <th>Latitude</th>
-                <th>Longitude</th>
-                <th>Time</th>
-            </tr>
-
-            <?php foreach ($points as $key => $point) : ?>
+    <section>
+        <?php if ( $canAdd ) : ?>
+            <pre><?php print_r( $pointParams ); ?></pre>
+        <?php else : ?>
+            <table>
                 <tr>
-                    <td><?= $point[ 'lat' ] ?></td>
-                    <td><?= $point[ 'lon' ] ?></td>
-                    <td><?= $point[ 'time' ] ?></td>
+                    <th>Latitude</th>
+                    <th>Longitude</th>
+                    <th>Time</th>
                 </tr>
+
+                <?php foreach ($points as $key => $point) : ?>
+                    <tr>
+                        <td><?= $point[ 'lat' ] ?></td>
+                        <td><?= $point[ 'lon' ] ?></td>
+                        <td><?= $point[ 'time' ] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+
+            <div id="map"></div>
+        <?php endif; ?>
+    </section>
+
+    <script>
+        var map = L.map( 'map' );
+
+        // Define layers.
+        var tiles = L.tileLayer.provider( 'OpenStreetMap.BlackAndWhite' );
+        var latestLocation = L.circleMarker([
+            <?= $lastPoint[ 'lat' ]; ?>,
+            <?= $lastPoint[ 'lon' ]; ?>,
+            {
+                radius: 88
+            }
+        ]);
+        var trail = L.polyline([
+            <?php foreach ( $points as $key => $point ) : ?>
+                [
+                    <?= $point[ 'lat' ]; ?>,
+                    <?= $point[ 'lon' ]; ?>
+                ],
             <?php endforeach; ?>
-        </table>
-    <?php endif; ?>
+        ]);
+
+        // Add layers to map.
+        tiles.addTo( map )
+        trail.addTo( map );
+        latestLocation.addTo( map );
+
+        // Center map on the latest location.
+        map.setView([
+            <?= $lastPoint[ 'lat' ]; ?>,
+            <?= $lastPoint[ 'lon' ]; ?>
+        ], 14);
+    </script>
 </body>
 </html>
