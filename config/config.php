ea299ce059b50856a5a862cf8da482848f6671c9
Fabien Basmaison <fabien@arkhi.org>
2017-06-13 13:33:17 -0500

    Display 300 latest points by default.

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@ class Config
         '4bb5bb75608f468c0720d147962936c9',
     ];
 
-    static public $requestLimit = 25;
+    static public $requestLimit = 300;
     static public $requestOffset = 0;
 }
 
